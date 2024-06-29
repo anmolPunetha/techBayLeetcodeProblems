9bc37a1e9060ef7a1cf6cacd456d8e0aaa63a104
Anmol Punetha <60095765+anmolPunetha@users.noreply.github.com>
2024-06-29 09:43:15 +0530

    Update floodFill.cpp

--- graphs/floodFill.cpp
+++ graphs/floodFill.cpp
@@ -17,6 +17,29 @@ public:
             dfs(ii,jj,org_color,color,image);
         }
     }
+
+
+    void bfs(int sr, int sc, int org_color, int color, vector<vector<int>>& image){
+        queue<pair<int,int>>q; //pair=>p.first, p.second
+        q.push({sr,sc});
+        image[sr][sc]=color;
+
+        while(!q.empty()){
+            pair<int,int> f=q.front(); //gives the q front entitiy
+            q.pop(); //removes the fifo node
+
+            int i=f.first, j=f.second;
+
+            for(int k=0;k<4;k++){
+                int ii=i+dx[k], jj=j+dy[k];
+                if(ii<0 or jj<0 or ii>=r or jj>=c or image[ii][jj]!=org_color){
+                    continue;
+                }
+                q.push({ii,jj});
+                image[ii][jj]=color;
+            }
+        }
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         //dfs first
 
@@ -30,7 +53,8 @@ public:
 
         if(org_color==color)return image;
         
-        dfs(sr, sc, org_color, color, image);
+        //dfs(sr, sc, org_color, color, image);
+        bfs(sr,sc,org_color, color, image);
         return image;
     }
 };
